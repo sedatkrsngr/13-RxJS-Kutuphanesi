810b8cd241bd80dfdeb12ac4455a78b1c3092f56
sedatkrsngr <limitless_fearless@hotmail.com>
2021-06-03 20:57:26 +0300

    1-of-operatoru bitti

--- RxJSProject/src/app/app.component.ts
+++ RxJSProject/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'RxJSProject';
+
+  constructor() {
+    const values = of('sedat', 27, [1, 2, 3]); //of vermiş olduğumuz değerleri subscribe olunabilecek observable türüne dönüştürür
+    values.subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
