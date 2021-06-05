cfd904f71ceff66a085ef7ce5f0e1074fa4b060d
sedatkrsngr <limitless_fearless@hotmail.com>
2021-06-05 22:35:37 +0300

    3-Timer-Operatoru Bitti

--- RxJSProject/src/app/app.component.ts
+++ RxJSProject/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { interval } from 'rxjs';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,14 +8,14 @@ import { interval } from 'rxjs';
 })
 export class AppComponent {
   title = 'RxJSProject';
-
+  subscription: any;
   constructor() {
-    const publisher = interval(1000); //interval vermiş olduğumuz  sürede bir çalışır
-
-    publisher.subscribe(
+    const publisher = timer(1000, 2000); //interval gibi aslında bağlandıktan 1 sn sonra çalış ve her 2 sn de bir çalışmaya devam et demek
+    //2.kullanımı ise timer(1000); //timer subscribe olduktan sonra vermiş olduğumuz süre sonra 1 kere  çalış demek ve ardından tamamlanır
+    this.subscription = publisher.subscribe(
       //subscribe ile 3 fonk çalışır. aldığımız data fonk ,hata fonk ve veri alma işlemi bitince çalışcak fonk
       (data) => {
-        console.log('sedat ' + data);//data her 1 sn de b,r değişecek ve 0,1,2... sonsuza kadar devam eder 1s aralıklarla unscribe olana kadar
+        console.log('sedat ' + data); //data her 1 sn de b,r değişecek ve 0,1,2... sonsuza kadar devam eder 1s aralıklarla unscribe olana kadar
       },
       (err) => {
         console.log(err);
@@ -24,5 +24,9 @@ export class AppComponent {
         console.log('veri alma işlemi bitti');
       }
     );
+  }
+
+  stopSubscribe() {//html tarafında butona basarak veri almayı durduruyoruz
+    this.subscription.unsubscribe();
   }
 }
