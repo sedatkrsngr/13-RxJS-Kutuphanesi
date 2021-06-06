f8e099f6ddf199ffe05032336e17b9eb008ec8ba
sedatkrsngr <limitless_fearless@hotmail.com>
2021-06-06 18:31:14 +0300

    18-skipUntil-Operatoru Bitti

--- RxJSProject/src/app/app.component.ts
+++ RxJSProject/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { from, pipe } from 'rxjs';
-import { skip } from 'rxjs/operators';
+import { interval, pipe, timer } from 'rxjs';
+import { skipUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -13,10 +13,13 @@ export class AppComponent {
 
   constructor() {
     //pipe içerisinde sırasıyla çalışacak absorvable nesneleri barındırır.
+    //skipUntil Skipten farkı içerisindeki method aktif olana kadar önceki işlemleri çalışsa dahi yok sayar.
+    //aşağıdaki örnekte interval değer üretse dahi timer 5 sn olmadan bu değerleri yok sayar ve zamanı gelince sonraki değerleri işler. 4,5,6.. diye yakalar
+    //2. örnek mesela click eventini bağlayalım intervalın değerleri click tuşuna basılmadan değerlendirilmez. Basıldıktan sonra gelen değerleri kullanır.
 
-    var nums = from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-//skip belirtilen ilk n değeri yok sayar yani sonucumuz ilk 4 değeri es geçersek 5,6,7..10
-    nums.pipe(skip(4)).subscribe(
+const myTimer=interval(1000);//her bir sn bir çalışır ve değerleri ise 0,1,2,...
+
+    myTimer.pipe(skipUntil(timer(5000))).subscribe(
       //subscribe ile 3 fonk çalışır. aldığımız data fonk ,hata fonk ve veri alma işlemi bitince çalışcak fonk
       (data) => {
         console.log(data);
