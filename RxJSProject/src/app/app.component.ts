a634dbd5c9b8f0b840f0d008986a264bc841e9f2
sedatkrsngr <limitless_fearless@hotmail.com>
2021-06-06 22:15:43 +0300

    35-concat-Operatoru bitti

--- RxJSProject/src/app/app.component.ts
+++ RxJSProject/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { forkJoin } from 'rxjs';
-import {  } from 'rxjs/operators';
+import { concat, from } from 'rxjs';
+import { delay } from 'rxjs/operators';
 import { ajax } from 'rxjs/ajax';
 
 @Component({
@@ -12,14 +12,14 @@ export class AppComponent {
   title = 'RxJSProject';
   subscription: any;
 
-  //forkJoin->içerisindeki observablelerin hepsinin birden yayınlamaları bitince hepsinin tüm değerlerini kendisi yayınlıyor
+  //concat->observable değerler alır. Observable yayınlamaları sırasıyla yapar. Biri bitmeden diğerini yayınlamaz
 
   constructor() {
+    const values = from([1,2,3,4]);
+    const values2 = from([5,6,7,8]);
+    const values3 = from([9,10,11,12]);
 
-forkJoin({
-ilkistek: ajax.getJSON<any>("https://jsonplaceholder.typicode.com/todos/1"),
-ikinciistek:ajax.getJSON<any>("https://jsonplaceholder.typicode.com/posts/1")
-}).subscribe(
+    concat(values,values2,values3).subscribe(
       //subscribe ile 3 fonk çalışır. aldığımız data fonk ,hata fonk ve veri alma işlemi bitince çalışcak fonk
       (data) => {
         console.log(data);
