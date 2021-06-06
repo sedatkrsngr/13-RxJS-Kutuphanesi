01331765a541ed1a3b63fc27fd0eae3c8a4cdc35
sedatkrsngr <limitless_fearless@hotmail.com>
2021-06-06 18:15:22 +0300

    16-DebounceTime-Operatoru Bitti

--- RxJSProject/src/app/app.component.ts
+++ RxJSProject/src/app/app.component.ts
@@ -1,27 +1,31 @@
-import { Component } from '@angular/core';
-import { from, pipe } from 'rxjs';
-import { find, first,filter,last,single,distinct } from 'rxjs/operators';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { from, fromEvent, pipe } from 'rxjs';
+import { distinct, debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'RxJSProject';
   subscription: any;
 
+  @ViewChild('textSearch') //Viewchild HTml tarafındaki değerleri typescript tarafında yakalamaya yarar ve aşağıdakiyle ikilidir.
+  textSerch!: ElementRef;
+
   constructor() {
-    const myArray = from([5, 10, 10, 20, 100, 100, 300, 600]); //içine dizi alan of methodu gibi düşün fromu
-
     //pipe içerisinde sırasıyla çalışacak absorvable nesneleri barındırır.
-    //distinct ise gelen verideki tekrarlı verileri teke düşürür
-//
+  }
+  ngAfterViewInit(): void {//Html çalıştıktan sonra çalışır.Böylece #degerlerine ulaşabiliriz
 
-    myArray.pipe(distinct()).subscribe(
+    //From event ise eventimizde yapacağımız işlemleri belirtir. Mesela aşaıda texte değer girildiği anda çalışacak
+    //Değer girildiğinde her bastığımızda işlem yapmasın belli bir süre bastıklarımızı toplu olarak işleme dahil etsin
+    //istiyorsak o zaman debounceTime kullanırız. Mesela aşağıda 300 ms içinde yazılanları işleme sokar. Arama işlemlerinde kullanılabilir
+    fromEvent<KeyboardEvent>(this.textSerch.nativeElement, 'keyup').pipe(debounceTime(300)).subscribe(
       //subscribe ile 3 fonk çalışır. aldığımız data fonk ,hata fonk ve veri alma işlemi bitince çalışcak fonk
       (data) => {
-        console.log(data);
+        console.log((data.target as HTMLInputElement).value);
       },
       (err) => {
         console.log(err);
