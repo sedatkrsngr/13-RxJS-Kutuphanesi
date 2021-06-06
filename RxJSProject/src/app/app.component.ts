403a89830fe2c2d0c448afea86cd8f6dc243a3f2
sedatkrsngr <limitless_fearless@hotmail.com>
2021-06-06 18:22:28 +0300

    17-Skip-Operatoru Bitti

--- RxJSProject/src/app/app.component.ts
+++ RxJSProject/src/app/app.component.ts
@@ -1,31 +1,25 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { from, fromEvent, pipe } from 'rxjs';
-import { distinct, debounceTime } from 'rxjs/operators';
+import { Component } from '@angular/core';
+import { from, pipe } from 'rxjs';
+import { skip } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent {
   title = 'RxJSProject';
   subscription: any;
 
-  @ViewChild('textSearch') //Viewchild HTml tarafındaki değerleri typescript tarafında yakalamaya yarar ve aşağıdakiyle ikilidir.
-  textSerch!: ElementRef;
-
   constructor() {
     //pipe içerisinde sırasıyla çalışacak absorvable nesneleri barındırır.
-  }
-  ngAfterViewInit(): void {//Html çalıştıktan sonra çalışır.Böylece #degerlerine ulaşabiliriz
 
-    //From event ise eventimizde yapacağımız işlemleri belirtir. Mesela aşaıda texte değer girildiği anda çalışacak
-    //Değer girildiğinde her bastığımızda işlem yapmasın belli bir süre bastıklarımızı toplu olarak işleme dahil etsin
-    //istiyorsak o zaman debounceTime kullanırız. Mesela aşağıda 300 ms içinde yazılanları işleme sokar. Arama işlemlerinde kullanılabilir
-    fromEvent<KeyboardEvent>(this.textSerch.nativeElement, 'keyup').pipe(debounceTime(300)).subscribe(
+    var nums = from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+//skip belirtilen ilk n değeri yok sayar yani sonucumuz ilk 4 değeri es geçersek 5,6,7..10
+    nums.pipe(skip(4)).subscribe(
       //subscribe ile 3 fonk çalışır. aldığımız data fonk ,hata fonk ve veri alma işlemi bitince çalışcak fonk
       (data) => {
-        console.log((data.target as HTMLInputElement).value);
+        console.log(data);
       },
       (err) => {
         console.log(err);
