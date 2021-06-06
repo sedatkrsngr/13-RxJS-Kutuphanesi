faa9abc8e74dd62373b89e3b57210e65a10be984
sedatkrsngr <limitless_fearless@hotmail.com>
2021-06-06 17:44:38 +0300

    14-Single-Operatoru Bitti

--- RxJSProject/src/app/app.component.ts
+++ RxJSProject/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { from, pipe } from 'rxjs';
-import { find, first,filter,last } from 'rxjs/operators';
+import { find, first,filter,last,single } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -15,10 +15,10 @@ export class AppComponent {
     const myArray = from([5, 10, 15, 20, 50, 100, 300, 600]); //içine dizi alan of methodu gibi düşün fromu
 
     //pipe içerisinde sırasıyla çalışacak absorvable nesneleri barındırır.
-    //Last ise belirtiğimiz şarta göre son nesneyi getirir.
-    //Last -> firstin  tam tersidir ve veri yoksa hata fırlatır
+    //single ise belirtiğimiz şarta göre tek nesneyi getirir. Eğer şartı sağlayan birden fazla veri varsa uyarı verir,hata vermez.
+//
 
-    myArray.pipe(last((val) => val > 100)).subscribe(
+    myArray.pipe(single((val) => val==100)).subscribe(
       //subscribe ile 3 fonk çalışır. aldığımız data fonk ,hata fonk ve veri alma işlemi bitince çalışcak fonk
       (data) => {
         console.log(data);
