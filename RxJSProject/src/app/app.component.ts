02c01c86683a4f76c85a4ae75f8fb6291280c564
sedatkrsngr <limitless_fearless@hotmail.com>
2021-06-06 17:58:18 +0300

    15-Distinct-Operatoru Bitti

--- RxJSProject/src/app/app.component.ts
+++ RxJSProject/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { from, pipe } from 'rxjs';
-import { find, first,filter,last,single } from 'rxjs/operators';
+import { find, first,filter,last,single,distinct } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -12,13 +12,13 @@ export class AppComponent {
   subscription: any;
 
   constructor() {
-    const myArray = from([5, 10, 15, 20, 50, 100, 300, 600]); //içine dizi alan of methodu gibi düşün fromu
+    const myArray = from([5, 10, 10, 20, 100, 100, 300, 600]); //içine dizi alan of methodu gibi düşün fromu
 
     //pipe içerisinde sırasıyla çalışacak absorvable nesneleri barındırır.
-    //single ise belirtiğimiz şarta göre tek nesneyi getirir. Eğer şartı sağlayan birden fazla veri varsa uyarı verir,hata vermez.
+    //distinct ise gelen verideki tekrarlı verileri teke düşürür
 //
 
-    myArray.pipe(single((val) => val==100)).subscribe(
+    myArray.pipe(distinct()).subscribe(
       //subscribe ile 3 fonk çalışır. aldığımız data fonk ,hata fonk ve veri alma işlemi bitince çalışcak fonk
       (data) => {
         console.log(data);
