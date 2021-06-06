48ffb7ab364902126c1d9b29dfe220814c59ab0c
sedatkrsngr <limitless_fearless@hotmail.com>
2021-06-06 21:51:01 +0300

    33-StartWith-Operatoru Bitti

--- RxJSProject/src/app/app.component.ts
+++ RxJSProject/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { from, fromEvent, interval, merge, of } from 'rxjs';
-import {} from 'rxjs/operators';
+import { mergeAll, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -11,15 +11,16 @@ export class AppComponent {
   title = 'RxJSProject';
   subscription: any;
 
-  //merge->içerisine aldığı absorvable datalarını birleştirir
+  //startWith->arrayin ilk indexine data koymak için kullanılır
+
   constructor() {
     const myArray = from([1,2,3]);
-    const myNum = of(15);
-    merge(myArray,myNum).subscribe(
+
+    myArray.pipe(startWith(4,5,6)).subscribe(
       //subscribe ile 3 fonk çalışır. aldığımız data fonk ,hata fonk ve veri alma işlemi bitince çalışcak fonk
       (data) => {
         console.log(data);
-        //1,2,3,15
+        //4,5,6,1,2,3
       },
       (err) => {
         console.log(err);
