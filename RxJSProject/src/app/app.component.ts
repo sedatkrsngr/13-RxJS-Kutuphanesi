157e5ac38d89d6d1e444116dd0f6b652cd50ee32
sedatkrsngr <limitless_fearless@hotmail.com>
2021-06-07 10:37:41 +0300

    45-BehaviorSubject-Operatoru Bitti

--- RxJSProject/src/app/app.component.ts
+++ RxJSProject/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {
-  AsyncSubject,
+  AsyncSubject, BehaviorSubject,
 } from 'rxjs';
 import {
 } from 'rxjs/operators';
@@ -15,14 +15,12 @@ export class AppComponent {
   title = 'RxJSProject';
   subscription: any;
 
-  //asyncsubject->subscribe olan yapılara en son yayınlanmış olan datayı gönderir.
-  //önemlik olan şu mutlaka complete methodu çalışmış olmalı
-  //aşağıdaki örn. complete 2. DEğer yazanda çalıştığı için 2 si de 2.değer yazısını görür
+  //behaviorsubject->ilk oluşturulduğu anda mutlaka ilk değeri alır
+  //önce subs sonra subs farketmez hepsine sonraki dataya kadar dataları yayar
   constructor() {
 
-    const myObservableSub = new AsyncSubject();
+    const myObservableSub = new BehaviorSubject("Başlangıç Değer");
 
-    myObservableSub.next("1. Değer");
 
     myObservableSub.subscribe(
       //subscribe ile 3 fonk çalışır. aldığımız data fonk ,hata fonk ve veri alma işlemi bitince çalışcak fonk
