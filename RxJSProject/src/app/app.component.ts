a75a132f02d49bf59096f75f82cfb7f223a0faf6
sedatkrsngr <limitless_fearless@hotmail.com>
2021-06-07 08:58:48 +0300

    38-finalize-Operatoru bitti

--- RxJSProject/src/app/app.component.ts
+++ RxJSProject/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { concat, from, fromEvent } from 'rxjs';
-import { delay, delayWhen, tap } from 'rxjs/operators';
+import { delay, delayWhen, finalize, tap, take } from 'rxjs/operators';
 import { ajax } from 'rxjs/ajax';
 
 @Component({
@@ -12,21 +12,25 @@ export class AppComponent {
   title = 'RxJSProject';
   subscription: any;
 
-  //delayWhen->Belirtilen fonksiyon çalışana kadar bekle demek
-  //Aşağıdaki örnekte ekranın herhangi bir yerine tıklayana kadar veri dinleme işi gerçekleşmez
+  //finalize->pipe içerisinde data çekme işlemi bittikten sonra çalışmasını istediğimiz methodları buraya yazarız. Subscribe 3 methodu bittikten sonra bu method çalışır
 
   constructor() {
-    ajax.getJSON<any>('https://jsonplaceholder.typicode.com/todos/1').pipe(delayWhen(val=>fromEvent(document,"click"))).subscribe(
-      //subscribe ile 3 fonk çalışır. aldığımız data fonk ,hata fonk ve veri alma işlemi bitince çalışcak fonk
-      (data) => {
-        console.log(data);
-      },
-      (err) => {
-        console.log(err);
-      },
-      () => {
-        console.log('veri alma işlemi bitti');
-      }
-    );
+    ajax
+      .getJSON<any>('https://jsonplaceholder.typicode.com/todos/1')
+      .pipe(
+        finalize(() => console.log('veri alma işleminden bittiden sonra bitti'))
+      )
+      .subscribe(
+        //subscribe ile 3 fonk çalışır. aldığımız data fonk ,hata fonk ve veri alma işlemi bitince çalışcak fonk
+        (data) => {
+          console.log(data);
+        },
+        (err) => {
+          console.log(err);
+        },
+        () => {
+          console.log('veri alma işlemi bitti');
+        }
+      );
   }
 }
