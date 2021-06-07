286861904f56d51167ef81399d425b81a930a3c0
sedatkrsngr <limitless_fearless@hotmail.com>
2021-06-07 10:25:00 +0300

    43-Subject-Operatoru Bitti

--- RxJSProject/src/app/app.component.ts
+++ RxJSProject/src/app/app.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
-import { concat, from, fromEvent, interval, of } from 'rxjs';
+import {
+  concat,
+  from,
+  fromEvent,
+  interval,
+  observable,
+  Observable,
+  of,
+  Subject,
+} from 'rxjs';
 import {
   delay,
   delayWhen,
@@ -24,31 +33,44 @@ export class AppComponent {
   title = 'RxJSProject';
   subscription: any;
 
-  //retryWhen->retryden farkı içerisine observable nesne alır. Her seferinde tekrar eder
-
+  //subject->observable nesnelerinin özelleştirilmiş tipidir. Observable nesnelerinin yetersiz kaldığı yerde devreye girerler
+  //İçinde çalıştırdığımız method tüm subscribler için ortaktır. Yani diyelim ki rastgele bir sayı üretiyor. Bunu 10 tane sub
+  //dinliyorsa hepsi aynı veriyi görür ama new observable deseydik her seferinde farklı veri görecektik
+  //Mutlaka next methodundan önce subscribe olması gerekiyor
   constructor() {
-    let myInterval = interval(1000)
-      .pipe(
-        map((val) => {
-          if (val > 6) {
-            throw "değer 6'dan büyük";
-          } else {
-            return val;
-          }
-        })
-      );
 
-      myInterval.pipe(retryWhen(err=>err.pipe(tap(x=>console.log(x))))).subscribe(
-        //subscribe ile 3 fonk çalışır. aldığımız data fonk ,hata fonk ve veri alma işlemi bitince çalışcak fonk
-        (data) => {
-          console.log(data);
-        },
-        (err) => {
-          console.log(err);
-        },
-        () => {
-          console.log('veri alma işlemi bitti');
-        }
-      );
+    const myObservable = new Observable(x=>{
+      x.next(Math.random());
+    });
+
+    const myObservableSub = new Subject();
+
+
+
+    myObservableSub.subscribe(
+      //subscribe ile 3 fonk çalışır. aldığımız data fonk ,hata fonk ve veri alma işlemi bitince çalışcak fonk
+      (data) => {
+        console.log(data);
+      },
+      (err) => {
+        console.log(err);
+      },
+      () => {
+        console.log('veri alma işlemi bitti');
+      }
+    );
+    myObservableSub.subscribe(
+      //subscribe ile 3 fonk çalışır. aldığımız data fonk ,hata fonk ve veri alma işlemi bitince çalışcak fonk
+      (data) => {
+        console.log(data);
+      },
+      (err) => {
+        console.log(err);
+      },
+      () => {
+        console.log('veri alma işlemi bitti');
+      }
+    );
+    myObservableSub.next(Math.random());
   }
 }
